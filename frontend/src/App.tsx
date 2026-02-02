14a103bace11f2eed38b39f9fe8a451ba7dbd05b
Justus Jones <justus1274@gmail.com>
2026-02-02 12:56:36 -0800

    feat: Add ScrollArea UI component and set up initial application routing.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import { Routes, Route } from 'react-router-dom'
+import { ScrollArea } from '@/components/ui/scroll-area'
 import {
   LandingPage,
   LoginPage,
@@ -12,19 +13,21 @@ import {
 
 function App() {
   return (
-    <Routes>
-      {/* Public routes */}
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/login" element={<LoginPage />} />
-      <Route path="/event/new" element={<AnonymousEventPage />} />
+    <ScrollArea className="h-screen w-screen">
+      <Routes>
+        {/* Public routes */}
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/event/new" element={<AnonymousEventPage />} />
 
-      {/* Protected routes (will add auth wrapper later) */}
-      <Route path="/dashboard" element={<DashboardPage />} />
-      <Route path="/teams" element={<TeamsPage />} />
-      <Route path="/teams/new" element={<CreateTeamPage />} />
-      <Route path="/events" element={<EventsPage />} />
-      <Route path="/events/new" element={<CreateEventPage />} />
-    </Routes>
+        {/* Protected routes (will add auth wrapper later) */}
+        <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/teams" element={<TeamsPage />} />
+        <Route path="/teams/new" element={<CreateTeamPage />} />
+        <Route path="/events" element={<EventsPage />} />
+        <Route path="/events/new" element={<CreateEventPage />} />
+      </Routes>
+    </ScrollArea>
   )
 }
 
